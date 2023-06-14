60c69dd945c902089dfc07e067dce59ab6b4837d
PedrobyJoao <joaoexplorer235@gmail.com>
2023-06-13 22:36:51 -0300

    fix: now it's really fixed

---
+++ nvim/lua/configs/keybinds.lua
@@ -0,0 +1,116 @@
+local function map(m, k, v)
+    vim.keymap.set(m, k, v, { noremap = true, silent = true })
+end
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Fix n and N. Keeping cursor in center
+map('n', 'n', 'nzz')
+map('n', 'N', 'Nzz')
+
+-- Enter normal mode faster
+map('i', 'jk', '<ESC>')
+map('i', 'kj', '<ESC>')
+map('i', 'KJ', '<ESC>')
+map('i', 'JK', '<ESC>')
+
+-- Delete faster
+map('n', '<leader>d', 'dd')
+
+-- Maintain cursor in the center while ctrl + u/d
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+
+-- Faster: search text
+map('n', '<leader>s', '/')
+
+-- Don't yank when pasting
+map("v", "p", '"_dP', opts)
+
+-- Nohl command to CRTL + N
+map('n', '<C-N>', ':nohl<CR>')
+
+-- Indent with tab
+map("v", "<S-Tab>", "<gv")
+map("v", "<Tab>", ">gv")
+map("n", "<S-Tab>", "<<")
+map("n", "<Tab>", ">>")
+
+-- Replace word where the cursor is
+map("n", "<leader>r", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
+
+-- Quickly save the current buffer or all buffers
+map('n', '<leader>w', '<CMD>update<CR>')
+map('n', '<leader>W', '<CMD>wall<CR>')
+
+-- Quit neovim
+map('n', '<leader>q', '<CMD>q<CR>')
+
+-- Quit and Save neovim
+map('n', '<leader>wq', '<CMD>wq<CR>')
+
+-- leader-o/O inserts blank line below/above
+map('n', '<leader>o', 'o<ESC>')
+map('n', '<leader>O', 'O<ESC>')
+
+-- Move line up and down in NORMAL and VISUAL modes
+-- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
+map('n', '<A-j>', '<CMD>move .+1<CR>')
+map('n', '<A-k>', '<CMD>move .-2<CR>')
+map('x', '<A-j>', ":move '>+1<CR>gv=gv")
+map('x', '<A-k>', ":move '<-2<CR>gv=gv")
+
+-- Use operator pending mode to visually select the whole buffer
+-- e.g. dA = delete buffer ALL, yA = copy whole buffer ALL
+map('o', 'A', ':<C-U>normal! mzggVG<CR>`z')
+map('x', 'A', ':<C-U>normal! ggVG<CR>')
+
+---------------------- Files ----------------------
+
+-- Easy access for file tree
+map('n', '<leader>e', ':NvimTreeFindFileToggle!<CR>')
+
+-- Close buffer
+map('n', '<leader>cb', ':bd<CR>')
+
+---------------------- Windows and Buffers ----------------------
+
+-- Better window navigation
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+-- Resize with arrows
+map("n", "<C-Up>", ":resize -2<CR>")
+map("n", "<C-Down>", ":resize +2<CR>")
+map("n", "<C-Left>", ":vertical resize -2<CR>")
+map("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Navigate buffers
+map("n", "<S-l>", ":bnext<CR>")
+map("n", "<S-h>", ":bprevious<CR>")
+
+-- Move to last buffer
+map('n', "''", '<CMD>b#<CR>')
+
+-- Copying the vscode behaviour of making tab splits
+map('n', '<C-\\>', '<CMD>vsplit<CR>')
+map('n', '<A-\\>', '<CMD>split<CR>')
+
+---------------------- TMUX ----------------------
+
+map('n', 'C-h', '<CMD>TmuxNavigateLeft<CR>')
+map('n', 'C-l', '<CMD>TmuxNavigateRight<CR>')
+map('n', 'C-j', '<CMD>TmuxNavigateDown<CR>')
+map('n', 'C-k', '<CMD>TmuxNavigateUp<CR>')
+
+---------------------- Style ----------------------
+-- Change colorscheme to text style
+-- map("n", "<C-p>", ":colorscheme peachpuff<CR>")
---
+++ nvim/lua/configs/plugins.lua
@@ -0,0 +1,78 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd('packer.nvim')
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        "christoomey/vim-tmux-navigator",
+        lazy = false
+    }
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = {
+                {'nvim-lua/plenary.nvim'},
+        }
+    }
+
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+
+    use "EdenEast/nightfox.nvim"
+
+    -- Cmp 
+    use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
+    use { "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" } -- buffer completions
+    use { "hrsh7th/cmp-path", commit = "447c87cdd6e6d6a1d2488b1d43108bfa217f56e1" } -- path completions
+    use { "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" } -- snippet completions
+    use { "hrsh7th/cmp-nvim-lsp", commit = "3cf38d9c957e95c397b66f91967758b31be4abe6" }
+    use { "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" }
+
+    -- Snippets
+    use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
+    use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
+
+    use {
+      'VonHeikemen/lsp-zero.nvim',
+      requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+        {'saadparwaiz1/cmp_luasnip'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/cmp-nvim-lua'},
+
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},
+        {'rafamadriz/friendly-snippets'},
+      }
+    }
+
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+      require("toggleterm").setup()
+    end}
+
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
+
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+
+end)
