df1bcaebe5de9106fcf76c9bb30b13f9398915df
JPexplorer <jpexplorer@pop-os.localdomain>
2023-06-16 17:23:09 -0300

    refact(polybar): getting monitor dinamically

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,9 +7,12 @@ killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-# Check if HDMI-2 is connected
-if [[ $(xrandr -q | grep 'HDMI-2 connected') ]]; then
+# One-monitor usage setup. It will use External Monitor XOR Laptop screen
+HDMI_MONITOR=$(xrandr --query | grep 'HDMI.* connected' | cut -d ' ' -f1)
+if [[ HDMI_MONITOR ]]; then
+    export MONITOR=$HDMI_MONITOR
     polybar --reload bspwm-external -c $HOME/.config/polybar/config.ini &
 else
+    export MONITOR=$(xrandr --query | grep ' connected' | cut -d ' ' -f1)
     polybar --reload bspwm-laptop -c $HOME/.config/polybar/config.ini &
 fi
