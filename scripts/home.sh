8bba314132160287da6476fe13a12105b87e47d1
PedrobyJoao <joaoexplorer235@gmail.com>
2023-08-01 13:22:10 -0300

    fix: more changes for 4k allignment

--- scripts/home.sh
+++ scripts/home.sh
@@ -2,7 +2,7 @@
 
 # If the count is greater than 1, then an external monitor is connected
 if (xrandr --query | grep 'HDMI.* connected' | cut -d ' ' -f1); then
-    xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output HDMI-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal
+    xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output HDMI-2 --primary --mode 3840x2160 --rate 60 --pos 0x0 --rotate normal
     xrandr --dpi 160
 else
     xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output HDMI-2 --off
