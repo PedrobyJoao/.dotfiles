40af31c5478a510ca1695e76beb2c850cca621c0
JPexplorer <jpexplorer@pop-os.localdomain>
2023-08-30 09:24:06 -0300

    feat(nvim): super important shortcut go to implementation

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -23,6 +23,8 @@ require('telescope').setup{
     },
 }
 
+vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations, {})
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
